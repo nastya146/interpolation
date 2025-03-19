7df8773dd61f888d0af38a01a386eb4fdc316222
Anastasia Kolomiets <kolomiets.ae@phystech.edu>
2025-03-20 02:09:34 +0300

    Update Readme.md; Poetry setup.

--- demo/example1.py
+++ demo/example1.py
@@ -1,6 +1,8 @@
+import sys
+import os 
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+ '/../')
 import numpy as np
 import matplotlib.pyplot as plt
-import os 
 from datetime import datetime
 from src.lagrange_interpolation import lagrange_interpolation
 
